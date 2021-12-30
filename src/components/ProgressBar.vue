e7565fe9f8f62d57889d075a18b73c3d4077312d
hoangphuc <phucherose@gmail.com>
2021-12-30 12:09:34 +0700

    set up store, component

---
+++ src/components/ProgressBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <p class="progress-bar">Progress: {{ progress }}</p>
+</template>
+
+<script>
+export default {
+  name: "ProgressBar",
+  computed: {
+    progress() {
+      const state = this.$store.state;
+      const percentProgress = Math.round(
+        (state.todos.filter((todo) => todo.completed).length /
+          state.todos.length) *
+          100
+      );
+      return percentProgress;
+    },
+  },
+};
+</script>
+
+<style>
+.progress-bar {
+  padding: 5px;
+  text-align: center;
+  background: rgb(106, 248, 118);
+  color: black;
+}
+</style>
